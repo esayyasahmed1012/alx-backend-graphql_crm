516eaba71aabb7bd63ce8da1e237d4c98fac8762
esayyas <esayyasahmed1012@gmail.com>
2025-08-17 01:59:31 +0300

    schema.py

--- schema.py
+++ schema.py
@@ -3,7 +3,5 @@ import graphene
 class Query(graphene.ObjectType):
     hello = graphene.String()
 
-    def resolve_hello(root, info):
+    def resolve_hello(self, info):
         return "Hello, GraphQL!"
-
-schema = graphene.Schema(query=Query)
