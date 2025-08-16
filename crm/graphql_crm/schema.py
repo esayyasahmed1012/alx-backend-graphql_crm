6ca1290f31ec8db17c7c158ba336b1c7850f4d45
esayyas <esayyasahmed1012@gmail.com>
2025-08-16 23:34:24 +0300

    schema.py

--- crm/graphql_crm/schema.py
+++ crm/graphql_crm/schema.py
@@ -7,4 +7,13 @@ class Query(CRMQuery, graphene.ObjectType):
 class Mutation(CRMMutation, graphene.ObjectType):
     pass
 
+schema = graphene.Schema(query=Query, mutation=Mutation)import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
 schema = graphene.Schema(query=Query, mutation=Mutation)
