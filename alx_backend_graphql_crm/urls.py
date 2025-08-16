acd1dbc7900928d2bb6431024ab426b930b33608
esayyas <esayyasahmed1012@gmail.com>
2025-08-16 23:02:56 +0300

    urls.py

--- alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/urls.py
@@ -20,6 +20,5 @@ from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-]
+     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+ ]
